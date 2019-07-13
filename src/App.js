e65d78b6acd2f9aeb5f63d027f650733950901e1
thando mini <tzone85@gmail.com>
2019-07-13 12:48:49 +0200

    some basic operations and Event handlers

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    onClick() {
+        alert('clicked');
+    }
+
+    render(){
+        const list = [
+            'Item 1',
+            'Item 2',
+            'Another Item'
+        ];
+
+        return (
+            <div className="App">
+                <h1>
+                    {
+                        list.map(item => {
+                            return (
+                                <div onClick={this.onClick}>{item}</div>
+                            );
+                        })
+                    }
+                </h1>
+            </div>
+        );
+    }
 }
 
 export default App;
